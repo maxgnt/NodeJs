64354b03adf858a7eb6a112806bda4315928779e
maxgnt <155428888+maxgnt@users.noreply.github.com>
2025-02-26 12:26:59 +0100

    work on memory_exo

--- NPM_Projects/memory_exo.js
+++ NPM_Projects/memory_exo.js
@@ -1,35 +1,63 @@
-const readlineSync = require('readline-sync');
+import readlineSync from 'readline-sync';
 
 let cartes = [1, 2, 3, 4, 1, 2, 3, 4];
+let trouvées = [false, false, false, false, false, false, false, false];
+let vies = 5;
+console.log("Bienvenue dans le Memory ! Trouvez les paires.");
+console.log("Vous avez 5 vies pour trouver toutes les paires.");
 
-console.log("Bienvene dans le jeu de memory!");
-console.log("Cartes disponibles: ", cartes);
 
-let premiereCarte = null; // => premiere carte choisie par l'utilisateur
+
+function affichervies() {
+    let(i = 0; i < vies; i++) {
+        console.log("Vies restantes : " + vies);
+    }
+}
+
+function afficherCartes() {
+    let affichage = "";
+    for (let i = 0; i < cartes.length; i++) {
+        if (trouvées[i]) {
+            affichage += cartes[i] + " ";
+        } else {
+            affichage += "* ";
+        }
+    }
+    console.log("Cartes : " + affichage);
+}
+
+function choisirCarte() {
+    let choix;
+    while (true) {
+        choix = parseInt(readlineSync.question("Choisissez une carte (0-7) : "));
+        if (!isNaN(choix) && choix >= 0 && choix <= 7 && !trouvées[choix]) {
+            return choix;
+        }
+        console.log("Choix invalide, donnez un chiffre entre 0 et 7.");
+    }
+}
 
 function jouer() {
+    while (!trouvées.every(val => val)) {
+        afficherCartes();
+        let c1 = choisirCarte();
+        let c2 = choisirCarte();
 
-    let i = readlineSync.question("Entrez un numéro de carte (0-7): ");
+        console.log(`Cartes choisies : ${cartes[c1]} et ${cartes[c2]}`);
 
-    if (i < 0 || i >= cartes.length) {
-        console.log("Carte Invalide. Choisissez un nombre entre 0 et 7");
-        return jouer();
+        if (cartes[c1] === cartes[c2]) {
+            console.log("Bravo ! Paire trouvée !");
+            trouvées[c1] = trouvées[c2] = true;
+        } else {
+            console.log("Pas de paire !");
+        }
     }
-
-    console.log("Retornez une carte" + i + ": " + cartes[i]);
+    console.log(" Félicitations ! Toutes les paires sont trouvées ! ");
 }
 
-if (premiereCarte === null) {
-    premiereCarte = i;
-    console.log("Chosissez une autre carte");
-    return jouer();
-} else {
-    if (cartes[premiereCarte] === cartes[i]) {
-        console.log("Bravo! Vous avez trouvé une paire!");
-        cartes[premiereCarte] = null;
-        cartes[i] = null;
-    } else {
-        console.log("Dommage! Les cartes ne correspondent pas");
-    }
-    premiereCarte = null;
+if (vies === 0) {
+    console.log("Vous avez perdu !");
 }
+
+
+jouer();
